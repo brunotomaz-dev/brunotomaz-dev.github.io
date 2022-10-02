81f1f104477187baaa1c4fea12437b02c087091d
Bruno Tomaz <tomaz.bruno@gmail.com>
2022-10-02 02:37:29 -0300

    correções para adicionar projetos

---
+++ src/pages/Projetos.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import Header from '../components/Header';
+
+class Projetos extends React.Component {
+  render() {
+    return (
+      <div>
+        <Header />
+        <h3><a href="https://brunotomaz-dev.github.io/project-lessons-learned/" target="_blank" rel="noreferrer">Lessons Learned</a></h3>
+        <h3><a href="https://brunotomaz-dev.github.io/project-pixels-art/" target="_blank" rel="noreferrer">Pixels Art</a></h3>
+        <h3><a href="https://brunotomaz-dev.github.io/project-todo-list/" target="_blank" rel="noreferrer">Todo List</a></h3>
+      </div>
+    );
+  }
+}
+
+export default Projetos;
