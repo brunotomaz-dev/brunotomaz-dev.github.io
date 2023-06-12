b0775a32d608b869be410cceefd4d5c84bdc36a0
brunotomaz-dev <tomaz.bruno@gmail.com>
2023-06-12 19:42:10 -0300

    :sparkles: feat: Adicionado projeto app de delivery

--- src/data/backEnd.js
+++ src/data/backEnd.js
@@ -4,12 +4,23 @@ const dataBackEnd = [
     page: 'https://tfc-frontend-production-b912.up.railway.app/',
     github: 'https://github.com/brunotomaz-dev/TFC-Trybe-Futebol-Clube',
     tecnologias: ['NodeJS', 'Express', 'MySQL', 'Sequelize', 'Docker', 'Typescript'],
-    detalhes: (
+    detalhes:
       `Projeto desenvolvido com uma tabela com a classificação dos clubes
     e informações/edições de partidas. Focado no back-end, foi construída uma API.
     Para edição de partidas é necessário login, protegido por um token JWT e
-    criptografia com bcrypt.`
-    ),
+    criptografia com bcrypt.`,
+  },
+  {
+    nome: 'Projeto Delivery App',
+    page: 'https://front-end-production-4154.up.railway.app/',
+    github: 'https://github.com/brunotomaz-dev/project-delivery-app',
+    tecnologias: ['NodeJS', 'Express', 'MySQL', 'Sequelize', 'React', 'Javascript'],
+    detalhes:
+      `Projeto desenvolvido em grupo, tanto o front-end quanto o back-end.
+      Foi utilizado o banco de dados MySQL, com o ORM Sequelize.
+      O back-end foi desenvolvido com NodeJS e Express. Para criar um token foi
+      utilizado o JWT e para criptografar a senha foi utilizado o MD5.
+      O front-end foi desenvolvido com React.`,
   },
 ];
 
